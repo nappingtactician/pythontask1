23e01aecd633ae1894867b64546ba1e27c0a39a8
Pranal Zode <prpzode@gmail.com>
2021-06-20 10:36:07 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, session, render_template
+import sqlite3
+
+app = Flask(__name__)
+app.secret_key = "super_secret"
+
+@app.route("/", methods=['GET', 'POST'])
+def login():
+    result = [{"result": "sanitized"}, {"result": "unsanitized"}]
+    if request.method == 'POST':
+        userdata = request.form
+        username = userdata['username']
+        password = userdata['password']
+        con = sqlite3.connect('accounts.db')
+        cur = con.cursor()
+        sql = 'SELECT * FROM accounts WHERE username = ? AND password = ?'
+        cur.execute(sql, (username, password))
+        account = cur.fetchone()
+        # if account is None:
+        #     return False
+        if account:
+            session['logged'] = True
+            return result[0]
+        else:
+            return result[1]
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
