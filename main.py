a1ef287e1437ba6cdb002a15b8b905bdc074dbce
Pranal Zode <prpzode@gmail.com>
2021-06-23 12:41:02 +0530

    created new function and added payload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, session, render_template, redirect
+from flask import Flask, request, session, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -11,29 +11,24 @@ def hello():
 
 
 @app.route("/v1/sanitized", methods=['GET', 'POST'])
-def login():
+def payload():
     result = [{"result": "sanitized"}, {"result": "unsanitized"}]
     if request.method == 'POST':
-        userdata = request.form
-        username = userdata['username']
-        password = userdata['password']
+        data = request.get_json(force=True)
+        username = data["username"]
+        password = data["password"]
         con = sqlite3.connect('accounts.db')
         cur = con.cursor()
         sql = 'SELECT * FROM accounts WHERE username = ? AND password = ?'
         cur.execute(sql, (username, password))
         account = cur.fetchone()
-        # if account is None:
-        #     return False
         if account:
             session['logged'] = True
             return result[0]
         else:
             return result[1]
-    return render_template('login.html')
 
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
